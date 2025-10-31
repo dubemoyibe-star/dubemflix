4b845cb5adf9acea1ce8cecdab563da0b1a2c0dd
dubemoyibe-star <dubemoyibe@gmail.com>
2025-10-31 22:49:44 +0000

    made impportant changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const searchBtn = document.getElementById('search-btn')
 let containerEl = document.getElementById('container')
 const watchlistBtn = document.getElementById("watchlist-btn")
 
+
 searchBtn.addEventListener("click", function(){
   containerEl.innerHTML = "";
   const searchValue = document.getElementById('search-input').value.toLowerCase();
@@ -13,7 +14,7 @@ searchBtn.addEventListener("click", function(){
    fetch(`https://www.omdbapi.com/?i=${encodeURIComponent(item.imdbID)}&plot=short&apikey=70e18d7b`)
   .then(res => res.json())
   .then(detail => { 
-     containerEl.innerHTML += `<div class="movie" id="movie">
+     containerEl.innerHTML += `<div class="movie" id="movie" data-id="${detail.imdbID}">
       <img class="movie-img" src="${detail.Poster}" alt="Movie Poster">
       <div>
       <div class="movie-header">
@@ -41,10 +42,10 @@ searchBtn.addEventListener("click", function(){
 
 document.addEventListener("click", function(e){
   if(e.target.classList.contains('watchlist-btn')){
-    const movieEl = e.target.closest('.movie');
     let watchlist = JSON.parse(localStorage.getItem('watchlist') || '[]');
+     const movieEl = e.target.closest('.movie')
     watchlist.push(movieEl.outerHTML);
-    localStorage.setItem('watchlist', JSON.stringify(watchlist));
+    localStorage.setItem('watchlist', JSON.stringify(watchlist))
     e.target.innerText = "✔ Added";
     e.target.disabled = true;
   }
--- watchlist.js
+++ watchlist.js
@@ -1,10 +1,43 @@
-const logoContainer = document.getElementById('logo-container');
-const watchlist = JSON.parse(localStorage.getItem('watchlist') || '[]');
-const button = document.getElementById('watchlist-btn');
+document.addEventListener("DOMContentLoaded", function(){
+  const buttons = document.querySelectorAll("button")
+buttons.forEach(button => {
+ button.innerText = "➖remove"
+ button.style.fontSize = "0.8em"
+ button.style.fontWeight = "700"
+})
+})
 
-if (watchlist.length === 0) {
-    logoContainer.innerHTML = `<p class="logo">Your watchlist is looking a little empty...<br>➕Let's add some movies</p>`;
-} else {
-    button.innerText = "✔ Added";
-    logoContainer.innerHTML = watchlist.join('');
-}
+function rendermovie(){
+  const logoContainer = document.getElementById('logo-container');
+   let watchlist = JSON.parse(localStorage.getItem('watchlist') || '[]');
+    localStorage.setItem('watchlist', JSON.stringify(watchlist))
+    logoContainer.innerHTML = ""
+    logoContainer.innerHTML = watchlist.join("")
+    
+    const buttons = document.querySelectorAll("button")
+    buttons.forEach(button => {
+    button.innerText = "➖remove"
+    button.style.fontSize = "0.8em"
+    button.style.fontWeight = "700"
+})
+  }
+
+rendermovie()
+
+
+
+
+document.addEventListener("click", function(e){
+  if(e.target.classList.contains('watchlist-btn')){
+    let watchlist = JSON.parse(localStorage.getItem('watchlist') || '[]');
+    const movieEl = e.target.closest('.movie');
+    const movieHTML = movieEl.outerHTML; 
+    const index = watchlist.indexOf(movieHTML);
+    if(index !== -1){
+      watchlist.splice(index, 1)
+      localStorage.removeItem("watchlist.splice(index, 1)");
+    }
+    localStorage.setItem('watchlist', JSON.stringify(watchlist));
+    movieEl.remove();
+  }
+});
